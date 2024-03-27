4c78978a5608143de0fb3a9ab6bcfa4f6cf44275
Zan Baldwin <hello@zanbaldwin.com>
2024-03-27 22:16:53 +0100

    Static Analysis Fixes

--- src/Formatter/NativeFormatter.php
+++ src/Formatter/NativeFormatter.php
@@ -15,7 +15,14 @@ class NativeFormatter implements ProtocolFormatterInterface
         if (\is_string($binary)) {
             $length = MbString::getLength($binary);
             if ($length === 16 || $length === 4) {
-                return \inet_ntop(\pack('A' . (string) $length, $binary));
+                $pack = \pack(\sprintf('A%d', $length), $binary);
+                // $pack return type is `string|false` below PHP 8 and `string`
+                // above PHP 8.
+                // @phpstan-ignore-next-line identical.alwaysFalse
+                if (false === $pack || false === $protocol = \inet_ntop($pack)) {
+                    throw new FormatException($binary);
+                }
+                return $protocol;
             }
         }
         throw new FormatException($binary);
@@ -28,11 +35,17 @@ class NativeFormatter implements ProtocolFormatterInterface
     {
         if (\is_string($binary)) {
             if (\filter_var($binary, \FILTER_VALIDATE_IP, \FILTER_FLAG_IPV4)) {
-                $sequence = \unpack('a4', \inet_pton($binary));
+                $number = \inet_pton($binary);
+                if (false === $number || false === $sequence = \unpack('a4', $number)) {
+                    throw new FormatException($binary);
+                }
                 return \current($sequence);
             }
             if (\filter_var($binary, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-                $sequence = \unpack('a16', \inet_pton($binary));
+                $number = \inet_pton($binary);
+                if (false === $number || false === $sequence = \unpack('a16', $number)) {
+                    throw new FormatException($binary);
+                }
                 return \current($sequence);
             }
             $length = MbString::getLength($binary);
