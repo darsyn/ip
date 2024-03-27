ddb7f15ca6a8599c23db3f181ec9223deea91fba
Zan Baldwin <hello@zanbaldwin.com>
2024-03-27 22:13:35 +0100

    Add PHPStan for Static Analysis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,6 @@ use PHPUnit\Framework\TestCase as TestCasePsr4;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-if (!class_exists(TestCasePsr4::class)) {
+if (!class_exists(TestCasePsr4::class) && class_exists(TestCasePsr1::class)) {
 	class_alias(TestCasePsr1::class, TestCasePsr4::class);
 }
